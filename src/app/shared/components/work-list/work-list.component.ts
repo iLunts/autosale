dde7e0ba0583e1f6462137516951b1eca14b833d
Roman Luntsevich <shipik2008@gmail.com>
2021-03-05 12:25:36 +0300

    Changed path

--- src/app/shared/components/work-list/work-list.component.ts
+++ src/app/shared/components/work-list/work-list.component.ts
@@ -8,37 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class WorkListComponent implements OnInit {
   workList: any[] = [
     {
-      img: '/assets/svg/work-icon.svg',
+      img: './assets/svg/work-icon.svg',
       title: 'Срочный выкуп авто',
       text: 'Быстрый выкуп авто - это удобно, деньги у Вас буквально через пару часов.',
       isMobile: true
     },
     {
-      img: '/assets/svg/work-icon.svg',
+      img: './assets/svg/work-icon.svg',
       title: 'Авто после ДТП',
       text: 'Состояние значения не имеет. От "немного поцарапана" до "полностью на запчасти".',
       isMobile: true
     },
     {
-      img: '/assets/svg/work-icon.svg',
+      img: './assets/svg/work-icon.svg',
       title: 'Аварийные авто',
       text: 'Выкупаем автомобили после серьезных аварий или пожара, эвакуатор за наш счет.',
       isMobile: true
     },
     {
-      img: '/assets/svg/work-icon.svg',
+      img: './assets/svg/work-icon.svg',
       title: 'Авто не на ходу',
       text: 'Нет желания "реанимировать" авто не на ходу - обращайтесь к нам, мы сами к Вам приедем за авто.',
       isMobile: true
     },
     {
-      img: '/assets/svg/work-icon.svg',
+      img: './assets/svg/work-icon.svg',
       title: 'Выкуп дорогих авто',
       text: 'Сложно продать дорогой автомобиль по хорошей цене - обращайтесь к нам.',
       isMobile: true
     },
     {
-      img: '/assets/svg/work-icon.svg',
+      img: './assets/svg/work-icon.svg',
       title: 'В любой точке страны',
       text: 'Наш оценщик готов бесплатно выехать в любую точку Беларуси, даже самую отдаленную.',
       isMobile: true
